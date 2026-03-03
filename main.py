563f082627e072164dbfcf483e9dc2886bd9b590
Mohamed-Elguindy <mohamedwelguindy@gmail.com>
2026-03-03 22:19:27 +0200

    pydantic and HTTPException

--- main.py
+++ main.py
@@ -1,60 +1,80 @@
-from fastapi import FastAPI
+from unittest.mock import DEFAULT
+
+from fastapi import FastAPI ,HTTPException
+from typing import Optional,List
+from enum import IntEnum
+from pydantic import BaseModel ,Field
 
 app = FastAPI()
 
+class Priority(IntEnum):
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+
+class todoBaseModel(BaseModel):
+    todoName: str = Field(...,min_length=3,max_length=100,description="Task Name")
+    priority: Priority = Field(default=Priority.LOW,description="Task priority")
+
+class Todo(todoBaseModel):
+    todoID: int = Field(...,description="Task ID")
+
+
+class todoCreate(todoBaseModel):
+    pass
+
+class todoUpdate(BaseModel):
+    todoName: Optional[str] = Field(None,min_length=3,max_length=100,description="Task Name")
+    priority: Optional[Priority] = Field(None,description="Task priority")
+
+
 allTodo = [
-    {"id": 1, "task": "Set up FastAPI project in IntelliJ", "priority": "High", "completed": True},
-    {"id": 2, "task": "Configure Virtualenv and install dependencies", "priority": "High", "completed": True},
-    {"id": 3, "task": "Create GET and POST endpoints for Todos", "priority": "Medium", "completed": False},
-    {"id": 4, "task": "Connect to a database (SQLAlchemy or Tortoise)", "priority": "Medium", "completed": False},
-    {"id": 5, "task": "Test endpoints using Swagger UI (/docs)", "priority": "Low", "completed": False}
+    Todo(todoID=1, todoName="Task 1", priority=Priority.MEDIUM),
+    Todo(todoID=2, todoName="Task 2", priority=Priority.MEDIUM),
+    Todo(todoID=3, todoName="Task 3", priority=Priority.HIGH),
+    Todo(todoID=4, todoName="Task 4", priority=Priority.LOW),
 ]
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-@app.get("/todos/{id}")
+@app.get("/todos/{id}" , response_model=Todo)
 def get_todo(id:int):
     for item in allTodo:
-        if item["id"] == id:
+        if item.todoID == id:
             return item
+    raise HTTPException(status_code=404, detail="Task not found")
 
-@app.get("/todos")
+@app.get("/todos", response_model=List[Todo])
 def get_todos(lastdo: int =None):
     if lastdo :
         return allTodo[:lastdo]
     return allTodo
 
-@app.post("/todos")
-def add_todo(todo : dict):
+@app.post("/todos", response_model=Todo)
+def add_todo(todo : todoCreate):
     lastdo = len(allTodo)+1
-    new_todo= {
-        "id": lastdo,
-        "task" : todo["task"],
-        "priortiy":todo["priority"],
-        "completed": todo["completed"]
-    }
-    allTodo.append(new_todo)
-    return todo
+    newTodo = Todo(todoID=lastdo, todoName=todo.todoName, priority =todo.priority)
+    allTodo.append(newTodo)
+    return newTodo
 
-@app.put("/todos/{id}")
-def edit_todo(id:int ,updated_todo : dict):
+@app.put("/todos/{id}" , response_model=Todo)
+def edit_todo(id:int ,updated_todo : todoUpdate):
     for item in allTodo:
-        if item["id"] == id:
-            item["task"]= updated_todo["task"]
-            item["priority"] = updated_todo["priority"]
-            item["completed"] = updated_todo["completed"]
-    return updated_todo
+        if item.todoID == id:
+            if updated_todo.todoName is not None:
+               item.todoName = updated_todo.todoName
+            if updated_todo.priority is not None:
+                item.priority = updated_todo.priority
+            return item
+        else:
+            raise HTTPException(status_code=404, detail="Task not found")
 
 @app.delete('/todos/{todo_id}')
 def delete_todo(todo_id: int):
     for index, todo in enumerate(allTodo):
-        if todo['id'] == todo_id:
+        if todo.todoID == todo_id:
             deleted_todo = allTodo.pop(index)
             return deleted_todo
-    return "Error, not found"
+    raise HTTPException(status_code=404, detail="Task not found")
